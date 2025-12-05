a53d0797f2d21c37de47ff12ac8964f568d62d7a
LapShop <ahmedaboraya110@gmail.com>
2025-12-05 17:42:33 +0200

    kotlin fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAvTRWCdvuX7Wp9tfMaCqANAeAX3inoQcE")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
